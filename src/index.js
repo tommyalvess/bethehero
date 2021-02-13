6db41c8f19a5578c53e6658535b8077d40174d18
Tom Alves <eliton.alves_santos@hotmail.com>
2021-02-13 20:37:02 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 app.use(routes); 
 app.use(errors());
 
