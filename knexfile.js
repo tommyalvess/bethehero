301be996fc70c5380755bbba19f8a3927a5890a0
Tom Alves <eliton.alves_santos@hotmail.com>
2021-02-14 16:27:45 -0300

    Add files via upload

--- knexfile.js
+++ knexfile.js
@@ -29,8 +29,13 @@ module.exports = {
   },
 //ambiente para o cliente acessar 
   production: {
-    client: 'postgres',
-    connection: process.env.DATABASE_URL,
+    client: 'pg',
+    connection: {
+      host : 'ec2-67-202-63-147.compute-1.amazonaws.com',
+      user : 'jzsyfklxvbgemp',
+      password : 'f4c597801bc729884f6f1c07f4deb6f44e8609f139f03d38f950d6f099895c28',
+      database : 'dc4f3b5q6k6bsq'
+    },
     migrations: {
       directory: './src/database/migrations'
     }
