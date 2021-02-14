8303133466ec071c1d94ff3048fd25a5e94a02c1
Tom Alves <eliton.alves_santos@hotmail.com>
2021-02-14 17:09:33 -0300

    fixed ssl

--- knexfile.js
+++ knexfile.js
@@ -3,15 +3,19 @@
 module.exports = {
 //ambiente na nossa maquina
   development: {
-    client: 'sqlite3',
+    client: 'pg',
+    version: '7.2',
     connection: {
-      filename: './src/database/db.sqlite'
+      host : 'ec2-67-202-63-147.compute-1.amazonaws.com',
+      user : 'jzsyfklxvbgemp',
+      password : 'f4c597801bc729884f6f1c07f4deb6f44e8609f139f03d38f950d6f099895c28',
+      database : 'dc4f3b5q6k6bsq',    
+      ssl: { rejectUnauthorized: false }
     },
+    
     migrations: {
       directory: './src/database/migrations'
-    },
-    useNullAsDefault: true, // O padrão Sqlite não aceita inserção de default values.
-     
+    }
   },
 // simula a preodução p/ time dev possa testar online
   staging: {
@@ -21,8 +25,9 @@ module.exports = {
       database: 'dbapptransescolar',
       user:     'app_transescolar',
       password: 'app125517',
-      charset: 'utf8'
+      charset: 'utf8',
     },
+
     migrations: {
       directory: './src/database/migrations'
     }
@@ -30,12 +35,15 @@ module.exports = {
 //ambiente para o cliente acessar 
   production: {
     client: 'pg',
+    version: '7.2',
     connection: {
       host : 'ec2-67-202-63-147.compute-1.amazonaws.com',
       user : 'jzsyfklxvbgemp',
       password : 'f4c597801bc729884f6f1c07f4deb6f44e8609f139f03d38f950d6f099895c28',
-      database : 'dc4f3b5q6k6bsq'
+      database : 'dc4f3b5q6k6bsq',    
+      ssl: { rejectUnauthorized: false }
     },
+    
     migrations: {
       directory: './src/database/migrations'
     }
