3a5fa46018d1d993e4d91f6aa6a14c2c7b46b706
Tom Alves <eliton.alves_santos@hotmail.com>
2021-02-15 21:49:50 -0300

    correções do db

--- knexfile.js
+++ knexfile.js
@@ -18,13 +18,14 @@ module.exports = {
   },
 // simula a preodução p/ time dev possa testar online
   staging: {
-    client: 'mysql',
+    client: 'pg',
+    version: '7.2',
     connection: {
-      host : '160.153.92.200',
-      database: 'dbapptransescolar',
-      user:     'app_transescolar',
-      password: 'app125517',
-      charset: 'utf8'
+      host : 'ec2-67-202-63-147.compute-1.amazonaws.com',
+      user : 'jzsyfklxvbgemp',
+      password : 'f4c597801bc729884f6f1c07f4deb6f44e8609f139f03d38f950d6f099895c28',
+      database : 'dc4f3b5q6k6bsq',    
+      ssl: { rejectUnauthorized: false }
     },
     migrations: {
       directory: './src/database/migrations'
