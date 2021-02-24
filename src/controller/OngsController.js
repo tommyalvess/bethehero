1bcfbb8434fd6204d020bc7574880e10d5a96ea8
Tom Alves <eliton.alves_santos@hotmail.com>
2021-02-23 22:39:03 -0300

    Update OngsController.js

--- src/controller/OngsController.js
+++ src/controller/OngsController.js
@@ -51,7 +51,7 @@ module.exports = {
         }, (err) => {
             console.log(err);
             if(err)
-                return res.status(500).send({ error: 'Não foi possível enviar o email'});
+                return res.status(500).send({ error: 'Não foi possível enviar o email', id: {id});
 
             connection('ongs').insert({
                 id,
